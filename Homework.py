01c1891eeb0e4f1c59ebabe7adaafa5496b13b67
Oksana <oksanamanola@gmail.com>
2022-11-21 22:07:54 +0200

    Initial commit

---
+++ Homework.py
@@ -0,0 +1,43 @@
+from random import randint
+n=10
+array=list()
+for i in range (n):
+    array.append (randint(0,100))
+print(array)
+print("Всього виводить 10 чисел")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Homework5.py
@@ -0,0 +1,13 @@
+def incr(A, B, C):
+    history = {}
+A = 2
+B = 10
+C = 3
+while A <=B:
+    print(A)
+    A = A+3
+while A >=B:
+    print(A)
+    A = A - 3
+    print("Ура, товарищі! Історія інкременту 2,5,8, 11")
+incr(2, 10, 3)
